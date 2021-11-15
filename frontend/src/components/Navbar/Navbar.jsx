158210ae2f81507fbf14e7756c1f2c987f5be39e
kailash360 <kailashkejriwal21@gmail.com>
2021-11-16 04:10:19 +0530

    minor fix

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 function Navigation({search,setSearch}) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">Decentragram</Link>
+        <Link className="navbar-brand" to="/">Snap Stock</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
